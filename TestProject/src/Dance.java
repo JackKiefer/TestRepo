8f1ee15172de8e6bf1f158082cc23c32a590c1c6
jack kiefer <jackkiefer21@augustana.edu>
2022-09-27 10:10:03 -0500

    added dancing and saying goodbye

---
+++ TestProject/src/Dance.java
@@ -0,0 +1,9 @@
+
+public class Dance {
+
+	public void main() {
+		System.out.print("Watch those chickens dance!");
+	}
+}
+
+
--- TestProject/src/TestMain.java
+++ TestProject/src/TestMain.java
@@ -2,5 +2,6 @@
 public class TestMain {
 	public void main() {
 		System.out.print("Hello Chicken World!");
+		System.out.print("Goodbye");
 	}
 }
